9315258de186bd04502928cef7ce08b091a0636a
Inframatic <isteffy@hotmail.com>
2013-08-01 17:38:37 -0400

    Added border and picture to style

---
+++ app/controllers/controller.js
@@ -0,0 +1,3 @@
+function SimpleController($scope) {
+    $scope.interns = [{name:'Jesus Christ',industry:'comp sci'},{name:'GOD YWH', industry:'creator inc'},{name:'Allah',industry:'Developer'},{name:'Hamburglar',industry:'Mcdonalds'}]
+}
