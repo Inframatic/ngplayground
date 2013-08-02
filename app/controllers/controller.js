690929030799588a5624a4c3dab4b1ddb2e0fa30
Inframatic <isteffy@hotmail.com>
2013-08-02 11:40:10 -0400

    Added checkbox

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,3 +1,6 @@
 function SimpleController($scope) {
-    $scope.interns = [{name:'Jesus Christ',industry:'comp sci'},{name:'GOD YWH', industry:'creator inc'},{name:'Allah',industry:'Developer'},{name:'Hamburglar',industry:'Mcdonalds'}]
+    $scope.interns = [{name:'Jesus Christ',industry:'comp sci', paid: true, location: "Boston", company_size: 400},{name:'GOD YWH', industry:'creator inc', paid: false, location: "New York", company_size: 1100},{name:'Allah',industry:'Developer', paid: true, location: "Boston", company_size: 40},{name:'Hamburglar',industry:'Mcdonalds', paid: 'true', location: "New", company_size: 100000}]
+	$scope.paid = true;
+	$scope.    
 }
+
