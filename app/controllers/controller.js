9b40946002bff8540cfbd32639bef3f0c50eed45
Inframatic <isteffy@hotmail.com>
2013-08-02 19:51:22 -0400

    Added correct number of filter search bars and included placeholders

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -2,12 +2,12 @@ var myModule = angular.module('myApp', []);
 
 function BusinessCtrl($scope) {
     $scope.statusList = [ 
-      {"applicationStatus": 'Applied' },
-      {"applicationStatus": 'Not yet' } ];
+      {"paidvolunteer": 'Paid' },
+      {"paidvolunteer": 'Volunteer' } ];
     $scope.list = [
-      {name:'Jesus Christ',industry:'comp sci', paid: "yes", location: "Boston",    company_size: 400, "applicationStatus": 'Applied'},
-      {name:'GOD YWH', industry:'creator inc', paid: "no", location: "New York", company_size: 1100, "applicationStatus": 'Not yet'},
-      {name:'Allah',industry:'Developer', paid: "yes", location: "Boston", company_size: 40, "applicationStatus": 'Not yet'},
-      {name:'Hamburglar',industry:'Mcdonalds', paid: 'yes', location: "New", company_size: 100000, "applicationStatus": 'Not yet'}
+      {name:'Jesus Christ',industry:'comp sci', "paidvolunteer": 'Paid', location: "Boston",    company_size: 400, "applicationStatus": 'Applied'},
+      {name:'GOD YWH', industry:'creator inc', "paidvolunteer": 'Volunteer', location: "New York", company_size: 1100, "applicationStatus": 'Not yet'},
+      {name:'Allah',industry:'Developer', "paidvolunteer": 'Paid', location: "Boston", company_size: 40, "applicationStatus": 'Not yet'},
+      {name:'Hamburglar',industry:'Mcdonalds', "paidvolunteer": 'Volunteer', location: "New", company_size: 100000, "applicationStatus": 'Not yet'}
     	];
 }
