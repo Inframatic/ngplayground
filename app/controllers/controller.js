c9e3526dd32131e539dbf9a7e3440059af129a98
Inframatic <isteffy@hotmail.com>
2013-08-02 19:31:52 -0400

    Late commit. Added select dropdown. Ready to input actual list

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,6 +1,13 @@
-function SimpleController($scope) {
-    $scope.interns = [{name:'Jesus Christ',industry:'comp sci', paid: true, location: "Boston", company_size: 400},{name:'GOD YWH', industry:'creator inc', paid: false, location: "New York", company_size: 1100},{name:'Allah',industry:'Developer', paid: true, location: "Boston", company_size: 40},{name:'Hamburglar',industry:'Mcdonalds', paid: 'true', location: "New", company_size: 100000}]
-	$scope.paid = true;
-	$scope.    
-}
+var myModule = angular.module('myApp', []);
 
+function BusinessCtrl($scope) {
+    $scope.statusList = [ 
+      {"applicationStatus": 'Applied' },
+      {"applicationStatus": 'Not yet' } ];
+    $scope.list = [
+      {name:'Jesus Christ',industry:'comp sci', paid: "yes", location: "Boston",    company_size: 400, "applicationStatus": 'Applied'},
+      {name:'GOD YWH', industry:'creator inc', paid: "no", location: "New York", company_size: 1100, "applicationStatus": 'Not yet'},
+      {name:'Allah',industry:'Developer', paid: "yes", location: "Boston", company_size: 40, "applicationStatus": 'Not yet'},
+      {name:'Hamburglar',industry:'Mcdonalds', paid: 'yes', location: "New", company_size: 100000, "applicationStatus": 'Not yet'}
+    	];
+}
